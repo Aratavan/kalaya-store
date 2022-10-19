7918bbc3dc2aee6f9106fc6f07196fad6302ee56
Mahesh Gowda <mahesh.nagaraj0589@gmail.com>
2022-10-19 17:24:13 +0530

    track-order

--- src/modules/layout/components/cart/index.tsx
+++ src/modules/layout/components/cart/index.tsx
@@ -3,7 +3,7 @@ const Cart = () => {
         <div className="cart flex flex-col justify-center align-middle px-[15px] text-center">
             <div className="icon flex flex-wrow justify-center cursor-pointer">
                 <img src="./Cart.svg" alt="profile-image" className="w-8" />
-                <span className="badge" style={{ position: 'absolute', top: '48px', padding: '0px 5px 5px 5px', borderRadius: '50%', background: '#9a3938', color: 'white', right: '90px' }}>
+                <span className="badge" style={{ position: 'absolute', top: '48px', padding: '0px 10px 5px 10px', fontSize: 'small', borderRadius: '50%', background: '#9a3938', color: 'white', right: '90px' }}>
                     3
                 </span>
             </div>
